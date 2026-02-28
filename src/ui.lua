f430802a550688103e8d5d9431fe689060f92052
makatymba2001 <makatymba2001@yandex.ru>
2026-02-28 15:12:32 +0200

    Nope, move it here

--- src/ui.lua
+++ src/ui.lua
@@ -104,7 +104,6 @@ end
 
 --
 
-
 SMODS.UIScrollBox = UIBox:extend()
 function SMODS.UIScrollBox:init(args)
     args = SMODS.merge_defaults(args, { content = {}, container = {}, overflow = {}, sync_mode = "progress" })
